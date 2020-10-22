70663341409dea87efdabf12929a7e15136f3ad1
TiDu <tidu.nguyen.2000@gmail.com>
2020-10-22 09:05:26 +0700

    added soutions to 0-1 sequences

--- 0-1_sequences/0-1_sequences.cpp
+++ 0-1_sequences/0-1_sequences.cpp
@@ -1,58 +1,93 @@
-//
-// Created by TiDu on 6/28/2020.
-//
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <vector>
-#include <map>
+#include <bits/stdc++.h>
 
-using str = std::string;
+using namespace std;
 
-std::map<int, int> findQuestionMark(str in_str) {
-    std::map<int, int> questionMarkMap;
-    for (int i = 0; i < in_str.size(); ++i) {
-        char c = in_str[i];
-        if (c == '?')
-            questionMarkMap.insert(std::make_pair(i, 0));
-    }
-    return questionMarkMap;
-}
+#define ar array
+#define ll long long
 
-str replaceQuestionMark(str in_str, std::map<int, int> &questionMarkMap) {
-    auto it = questionMarkMap.begin();
-    for (; it != questionMarkMap.end(); it++) {
-        int index = it->first, value = it->second;
-        in_str[index] = std::to_string(value)[0];
-    }
-    return in_str;
-}
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
 
-int calNSwap(str in_str) {
-    return 0;
-}
-
-int main() {
-    str inpStr;
-    std::cin >> inpStr;
-    std::map<int, int> questionMarkMap = findQuestionMark(inpStr);
-    int nSwap = 0;
-    int cases = pow(2, questionMarkMap.size());
-    auto it = questionMarkMap.end();
-    while (cases--) {
-        std::cout << cases << '\n';
-        if (cases % 2 == 0) {
-            questionMarkMap[it->first] = 1;
-            if (it != questionMarkMap.begin())
-                it--;
+int count_inversion(string s)
+{
+    int cur_non_0 = 0, n_inversion = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '0')
+        {
+            n_inversion += i - cur_non_0;
+            cur_non_0++;
         }
-
-        str replacedStr = replaceQuestionMark(inpStr, questionMarkMap);
-        std::cout << replacedStr << '\n';
-        nSwap += calNSwap(replacedStr);
     }
-    std::cout << nSwap % 1000000007;
-    return 0;
+    return n_inversion;
 }
 
+void solve()
+{
+    using std::map;
+    string s;
+    cin >> s;
+    ll sum = 0;
 
+    map<int, char> unknown_pos;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '?')
+        {
+            unknown_pos[i] = '0';
+        }
+    }
+
+    int n_s = pow(2, unknown_pos.size());
+    auto it = unknown_pos.begin();
+
+    for (int i = 0; i < n_s; i++)
+    {
+        string permutation = s;
+
+        for (auto p : unknown_pos)
+            permutation[p.first] = p.second;
+
+        sum += count_inversion(permutation);
+
+        if (i < n_s - 1)
+        {
+            if (it->second == '0')
+            {
+                it->second = '1';
+            }
+            else
+            {
+                while (it->second == '1' && it != unknown_pos.end())
+                {
+                    it++;
+                }
+                it->second = '1';
+                while (it != unknown_pos.begin())
+                {
+                    --it;
+                    it->second = '0';
+                }
+            }
+        }
+    }
+
+    cout << sum % MOD << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n_test;
+    cin >> n_test;
+    while (n_test--)
+    {
+        solve();
+    }
+}
